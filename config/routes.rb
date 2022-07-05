6a0eadbaf8e5e9cfa27d93b26505ae3cd5f64d99
EC2 Default User <ec2-user@ip-172-31-95-15.ec2.internal>
2022-07-05 23:31:13 +0000

    [update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,66 +1,29 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'sessions/new'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/complete'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/quit'
-    get 'customers/leave'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'registrations/new'
-    get 'registrations/create'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :admins
   devise_for :customers
+  devise_for :admins
+  
+  get 'homes/top'
+  get 'homes/about'
+  
+  resources :addresses, only: [:index, :edit]
+  resources :orders, only: [:new, :index, :show]
+  get 'orders/complete'
+  resources :cart_items, only: [:index]
+  resources :customers, only: [:show, :edit, :update]
+  get 'customers/quit'
+  get 'customers/leave'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :registrations, only: [:new, :create]
+  resources :items, only: [:index, :show]
+  
+  
+  namespace :admin do
+    resources :orders, only: [:show]
+    resources :customers, only: [:index, :show, :edit]
+    resources :genres, only: [:index, :edit]
+    resources :items, only: [:index, :new, :show, :edit]
+    resources :sessions, only: [:new]
+    get 'homes/top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
