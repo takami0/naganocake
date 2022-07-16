0f9edcd38cac2ea0fca5e3b12f0705ccc0bd11f1
EC2 Default User <ec2-user@ip-172-31-95-15.ec2.internal>
2022-07-16 23:02:37 +0000

    [add]item

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,11 +2,3 @@ class Genre < ApplicationRecord
   # belongs_to
   #enum name: {cake: 0, pudding: 1, baked: 2, candy: 3}
 end
-
-# [["テキスト1" 1], ["テキスト2", 2], ..]
-
-# rails c
-# Genre.all.map {|genre| [genre.name, genre.id]}
-#  [["ケーキ", 1], ["プリン", 2], ["焼き菓子", 3], ["キャンディ", 4]]
-
-#<%= f.select :name, Genre.all.map { |genre| [genre.name, genre.id] } %>
