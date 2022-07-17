fb2503bb847103e6fad73f31f7903acf9cd64e29
EC2 Default User <ec2-user@ip-172-31-95-15.ec2.internal>
2022-07-17 03:35:51 +0000

    [add]item fin

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,31 +1,39 @@
 class Admin::ItemsController < ApplicationController
   def index
-    @items = Item.page(page)
+    @items = Item.page(params[:page])
   end
 
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
-    if @item.save
+    if @item.save(item_params)
       redirect_to admin_items_path
     else
       render :new
     end
   end
-  
+
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
-  
+
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path
+    else
+      render :edit
+    end
   end
-  
+
   private
    def item_params
      params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,4 @@
 class Genre < ApplicationRecord
-  # belongs_to
+  belongs_to :Item
   #enum name: {cake: 0, pudding: 1, baked: 2, candy: 3}
 end
