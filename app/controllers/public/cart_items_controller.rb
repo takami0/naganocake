d4d45052a1a92e2008e23b2a49b58c43ce03a30e
EC2 Default User <ec2-user@ip-172-31-95-15.ec2.internal>
2022-07-24 02:33:37 +0000

    [add]cart

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -13,6 +13,18 @@ class Public::CartItemsController < ApplicationController
     end
   end
 
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to public_cart_items_path
+  end
+
+  def destroy_all
+    # cart_item = current_customer.cart_item.all
+    current_customer.cart_items.destroy_all
+    redirect_to public_cart_items_path
+  end
+
   private
   def cart_item_params
     params.require(:cart_item).permit(:amount, :item_id)
